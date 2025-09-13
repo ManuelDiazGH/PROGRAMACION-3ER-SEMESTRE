8c56f260683bc5afaca9e2f56d884fbcbc5e6e06
ManuelDiazGH <150974800+ManuelDiazGH@users.noreply.github.com>
2025-09-13 20:02:23 -0300

    Add files via upload

---
+++ tp2 lombok y dto/src/main/java/tp2/Main.java
@@ -0,0 +1,21 @@
+package tp2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Usuario usuario1 = Usuario.builder()
+                .id(1L)
+                .nombre("Juan Perez")
+                .email("juanperez@gmail.com")
+                .build();
+
+        Usuario usuario2 = Usuario.builder()
+                .id(2L)
+                .nombre("María Lopez")
+                .email("marialopez@gmail.com")
+                .build();
+
+        System.out.println(usuario1);
+        System.out.println(usuario2);
+    }
+}
---
+++ tp2 lombok y dto/src/main/java/tp2/Usuario.java
@@ -0,0 +1,12 @@
+package tp2;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Usuario {
+    private Long id;
+    private String nombre;
+    private String email;
+}
