e4b31ef960934b81d7bc2fc374d69094c736fd74
ManuelDiazGH <150974800+ManuelDiazGH@users.noreply.github.com>
2025-10-15 18:04:13 -0300

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-const PORT = process.env.PORT;
-const MODE = process.env.MODE;
-
-console.log(`Servidor iniciado en el puerto ${PORT} en modo ${MODE}`);
